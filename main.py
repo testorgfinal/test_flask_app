ca73d8b2f0e3334d8ee7bd90ad6469265c77944d
Himali Goel <himalig11@gmail.com>
2022-06-12 13:45:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 
 from flask import Flask
+import mysql.connector
+from mysql.connector import Error
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+try:
+    connection = mysql.connector.connect(host=os.getenv('HOST'),
+                                         database=os.getenv('DATABASE'),
+                                         user=os.getenv('USER'),
+                                         password=os.getenv('PASSWORD'))
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
